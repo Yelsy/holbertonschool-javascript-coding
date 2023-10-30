6df9efe323a6a3aa5e5aca3cf9d1d5faae3507d3
yelsy <yelsycg17@gmail.com>
2023-10-30 17:48:54 -0500

    feat: display the status code of a GET request

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+const path = process.argv[2];
+
+request(path, (err, res) => {
+  if (err) console.error(err);
+  else console.log(`code: ${res.statusCode}`)
+});
