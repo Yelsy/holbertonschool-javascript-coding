82df36bb11a9eb2f9e54402d81d218700626c43f
yelsy <yelsycg17@gmail.com>
2023-10-30 17:26:29 -0500

    feat: script that writes a string to a file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,6 +3,6 @@
 const fs = require('fs');
 const path = process.argv[2];
 const content = process.argv[3];
-fs.writeFile8(path, content, 'utf8', (err) => {
+fs.writeFile(path, content, 'utf8', (err) => {
   if (err) console.error(err);
 });
