6568b1d805acc86f9be671f31180469cebdce546
yelsy <yelsycg17@gmail.com>
2023-10-30 19:04:30 -0500

    feat:  print the number of movies where Wedge Antilles  is present

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  else {
+    const results = JSON.parse(body).results;
+    let count = 0;
+    for (let i = 0; i < results.length; i++) {
+      const characters = results[i].characters;
+      characters.foreach((id) => {
+        if (id.endswith('/18/')) count++;
+      })
+    }
+    console.log(count);
+  }
+})
