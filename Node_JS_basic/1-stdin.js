208b168d70de6b905b046ab7fa29cf1a652c374e
yelsy <yelsycg17@gmail.com>
2023-11-07 16:45:44 -0500

    feat: Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,19 +1,14 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.question('', (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  rl.close();
+process.stdin.on('data', (data) => {
+  const input = data.toString().trim();
+
+  if (input) {
+    console.log(`Your name is: ${input}`);
+    process.exit();
+  }
 });
 
-// Handle program closure
-rl.on('close', () => {
-  process.exit(0);
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
