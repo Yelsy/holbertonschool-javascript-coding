92d330e5209d04906e7b49ad883a1a04341ee406
yelsy <yelsycg17@gmail.com>
2023-11-07 16:47:35 -0500

    feat: Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-
-  if (input) {
-    console.log(`Your name is: ${input}`);
-    process.exit();
-  }
+process.stdin.on('data', (name) => {
+  process.stdout.write(`Your name is: ${name}`);
+  process.stdin.end();
 });
-
 process.on('exit', () => {
   console.log('This important software is now closing');
 });
