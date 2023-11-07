4dc550f2747ae55cf47f5c13fa9e11dedad8a653
yelsy <yelsycg17@gmail.com>
2023-11-06 22:13:38 -0500

    feat:  prints all characters of a Star Wars movie

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -1,32 +1,30 @@
 #!/usr/bin/node
 const request = require('request');
 
-const movieId = process.argv[2]; // Get the Movie ID from the command line argument
+const movieId = process.argv[2];
 
 if (!movieId) {
-  console.log('Please provide a Movie ID as a command line argument.');
+  console.log('Debes proporcionar el ID de la película como argumento.');
   process.exit(1);
 }
 
 const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
 request(apiUrl, (error, response, body) => {
-  if (error || response.statusCode !== 200) {
-    console.error('Error:', error);
-  } else {
+  if (!error && response.statusCode === 200) {
     const movieData = JSON.parse(body);
-    const characterUrls = movieData.characters;
 
-    // Fetch character data for each character URL
-    characterUrls.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const characterData = JSON.parse(body);
+    movieData.characters.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (!charError && charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
           console.log(characterData.name);
         } else {
-          console.error('Error:', error);
+          console.error('Error getting character data:', charError);
         }
       });
     });
+  } else {
+    console.error('Error getting movie data:', error);
   }
 });
