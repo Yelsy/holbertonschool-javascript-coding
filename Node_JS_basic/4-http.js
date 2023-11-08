dba081284265e58e22058d5e0d7b996b01ef6385
yelsy <yelsycg17@gmail.com>
2023-11-07 21:24:03 -0500

    feat: Create a small HTTP server using Node's HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello Holberton School!\n');
+});
+
+app.listen(1245);
+
+module.exports = app;
